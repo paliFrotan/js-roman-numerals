28a74faf286bdffb169b3577bda83d3a894f6499
Pali Frotan <pali.frotan@icloud.com>
2023-06-03 13:47:02 +0100

    Feat: roman-numeral handles I

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,4 +1,15 @@
-export const romanNumerals = (parameter) => {
-    if (parameter === undefined) throw new Error('Roman Numeral is required');
-    return "I";
+export const romanNumerals = (number) => {
+    if (number === undefined) throw new Error('Roman Numeral is required');
+    if (!Number.isInteger(number)) return "";
+    let answer = "";
+    const romanNumeralsObj = {
+        I: 1
+    };
+    for (const key in romanNumeralsObj) {
+        while (number >= romanNumeralsObj[key]) {
+          answer += key;
+          number -= romanNumeralsObj[key];
+        }
+    }
+    return answer;
 }
--- roman-numerals.test.js
+++ roman-numerals.test.js
@@ -3,8 +3,11 @@ import {romanNumerals} from "./roman-numerals.js";
   describe("romanNumerals", () => {
     test("returns roman-numeral from number", () => {
       expect(romanNumerals(1)).toBe("I");
+      expect(romanNumerals(2)).toBe("II");
     });
-    test("does nothing if roman-numeral not supplied", () => {
-      expect(romanNumerals()).toBe();
+    test("returns empty-string if roman-numeral not integer", () => {
+      expect(romanNumerals(0)).toBe("");
+      expect(romanNumerals(34.2)).toBe("");
+      expect(romanNumerals("3")).toBe("");
     });
   });
