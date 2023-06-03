c7eebd093095a5ceab7e8cf6b1bdf5d8cc62d891
Pali Frotan <pali.frotan@icloud.com>
2023-06-03 14:06:51 +0100

    Feat: RNs converted for number max 3999

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,11 +1,23 @@
 export const romanNumerals = (number) => {
     if (number === undefined) throw new Error('Roman Numeral is required');
-    if (!Number.isInteger(number)) return "";
+    if (!Number.isInteger(number) || number >= 4000) return "";
     let answer = "";
     const romanNumeralsObj = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV:4,
         I: 1
     };
-    for (const key in romanNumeralsObj) {
+    for (let key in romanNumeralsObj) {
         while (number >= romanNumeralsObj[key]) {
           answer += key;
           number -= romanNumeralsObj[key];
--- roman-numerals.test.js
+++ roman-numerals.test.js
@@ -5,6 +5,21 @@ import {romanNumerals} from "./roman-numerals.js";
       expect(romanNumerals(1)).toBe("I");
       expect(romanNumerals(2)).toBe("II");
     });
+    test("returns RN from edge cases", () => {
+      expect(romanNumerals(4)).toBe("IV");
+      expect(romanNumerals(5)).toBe("V");
+      expect(romanNumerals(6)).toBe("VI");
+    });
+    test("returns RN from edge cases", () => {
+      expect(romanNumerals(3999)).toBe("MMMCMXCIX");
+      expect(romanNumerals(4000)).toBe("");
+    });
+    test("returns RN from some further numbers", () => {
+      expect(romanNumerals(900)).toBe("CM");
+      expect(romanNumerals(17)).toBe("XVII");
+      expect(romanNumerals(250)).toBe("CCL");
+    });
+
     test("returns empty-string if roman-numeral not integer", () => {
       expect(romanNumerals(0)).toBe("");
       expect(romanNumerals(34.2)).toBe("");
